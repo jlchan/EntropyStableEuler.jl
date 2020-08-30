400659b5c9c132ab37b6fa74c0fd113c051cf9c7
Jesse Chan <jesse.chan@rice.edu>
2020-08-30 17:54:30 -0500

    passing tests

--- src/EntropyStableEuler.jl
+++ src/EntropyStableEuler.jl
@@ -12,12 +12,63 @@ const γ = 1.4
 export logmean
 include("./logmean.jl")
 
+# export entropy_scale,scale_entropy_vars_input,scale_entropy_vars_output
+# changes definition of entropy variables by a constant scaling
+const entropy_scale = 1/(γ-1)
+scale_entropy_vars_output(V...) = (x->@. x*entropy_scale).(V)
+scale_entropy_vars_input(V...) = (x->@. x/entropy_scale).(V)
+
+#####
+##### one-dimensional fluxes
+#####
+module Fluxes1D
+import ..γ
+import ..entropy_scale
+import ..EntropyStableEuler: logmean
+import ..EntropyStableEuler: scale_entropy_vars_output, scale_entropy_vars_input
+
+export primitive_to_conservative,conservative_to_primitive_beta
+export u_vfun, v_ufun
+
+# dispatch to n-dimensional constitutive routines, with optional entropy scaling
+function primitive_to_conservative(rho,u,p)
+   rho,rhou,E = primitive_to_conservative_nd(rho,tuple(u),p)
+   return rho,rhou[1],E
+end
+function v_ufun(rho,rhou,E)
+    v1,vU,vE = v_ufun_nd(rho,tuple(rhou),E)
+    return scale_entropy_vars_output(v1,vU[1],vE)
+end
+function u_vfun(v1,vU,vE)
+    v1,vU,vE = scale_entropy_vars_input(v1,vU,vE)
+    rho,rhoU,E = u_vfun_nd(v1,tuple(vU),vE)
+    return rho,rhoU[1],E
+end
+function conservative_to_primitive_beta(rho,rhou,E)
+    rho,U,beta = conservative_to_primitive_beta_nd(rho,tuple(rhou),E)
+    return rho,U[1],beta
+end
+
+export Sfun,pfun
+Sfun(rho,rhou,E) = Sfun_nd(rho,tuple(rhou),E)
+pfun(rho,rhou,E) = pfun_nd(rho,tuple(rhou),E)
+include("./entropy_variables.jl")
+
+export euler_fluxes_1D
+include("./euler_fluxes.jl")
+end
+
+
+
 #####
 ##### two-dimensional fluxes
 #####
 module Fluxes2D
 import ..γ
-using ..EntropyStableEuler # for logmean
+import ..entropy_scale
+import ..EntropyStableEuler: logmean, entropy_scale
+import ..EntropyStableEuler: scale_entropy_vars_output, scale_entropy_vars_input
+
 export primitive_to_conservative,conservative_to_primitive_beta
 export u_vfun, v_ufun
 
@@ -46,7 +97,47 @@ pfun(rho,rhou,rhov,E) = pfun_nd(rho,(rhou,rhov),E)
 include("./entropy_variables.jl")
 
 export euler_fluxes_2D, euler_fluxes_2D_x, euler_fluxes_2D_y
-include("./euler_fluxes_2D.jl")
+include("./euler_fluxes.jl")
+end
+
+#####
+##### three-dimensional fluxes
+#####
+module Fluxes3D
+import ..γ
+import ..entropy_scale
+import ..EntropyStableEuler: logmean, entropy_scale
+import ..EntropyStableEuler: scale_entropy_vars_output, scale_entropy_vars_input
+
+export primitive_to_conservative,conservative_to_primitive_beta
+export u_vfun, v_ufun
+
+# dispatch to n-dimensional constitutive routines, with optional entropy scaling
+function primitive_to_conservative(rho,u,v,w,p)
+   rho,rhoU,E = primitive_to_conservative_nd(rho,(u,v,w),p)
+   return rho,rhoU...,E
+end
+function v_ufun(rho,rhou,rhov,rhow,E)
+    v1,vU,vE = v_ufun_nd(rho,(rhou,rhov,rhow),E)
+    return scale_entropy_vars_output(v1,vU...,vE)
+end
+function u_vfun(v1,vU1,vU2,vU3,vE)
+    v1,vU1,vU2,vU3,vE = scale_entropy_vars_input(v1,vU1,vU2,vU3,vE)
+    rho,rhoU,E = u_vfun_nd(v1,(vU1,vU2,vU3),vE)
+    return rho,rhoU...,E
+end
+function conservative_to_primitive_beta(rho,rhou,rhov,rhow,E)
+    rho,U,beta = conservative_to_primitive_beta_nd(rho,(rhou,rhov,rhow),E)
+    return rho,U...,beta
+end
+
+export Sfun,pfun
+Sfun(rho,rhou,rhov,rhow,E) = Sfun_nd(rho,(rhou,rhov,rhow),E)
+pfun(rho,rhou,rhov,rhow,E) = pfun_nd(rho,(rhou,rhov,rhow),E)
+include("./entropy_variables.jl")
+
+export euler_fluxes_3D
+include("./euler_fluxes.jl")
 end
 
 # export u_vfun, v_ufun, betafun, pfun, rhoe_ufun
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,23 +8,66 @@ using Test
     @test logmean(uL,uL) ≈ uL
 end
 
+@testset "1D entropy variable tests" begin
+    using EntropyStableEuler.Fluxes1D
+    import EntropyStableEuler: γ,entropy_scale
+
+    rho,u,p = 1,.1,2
+    rho,rhou,E = Fluxes1D.primitive_to_conservative(rho,u,p)
+    v1,v2,v3 = Fluxes1D.v_ufun(rho,rhou,E)
+
+    h = 1e-7
+    central_diff(f,x) = (f(x+h) - f(x-h))/(2*h)
+    @test abs(v1 - central_diff(rho->Fluxes1D.Sfun(rho,rhou,E),rho)) < h
+    @test abs(v2 - central_diff(rhou->Fluxes1D.Sfun(rho,rhou,E),rhou)) < h
+    @test abs(v3 - central_diff(E->Fluxes1D.Sfun(rho,rhou,E),E)) < h
+
+    u1,u2,u3 = Fluxes1D.u_vfun(v1,v2,v3)
+    @test u1 ≈ rho
+    @test u2 ≈ rhou
+    @test u3 ≈ E
+
+    # test symmetry
+    UL = copy.((rho,rhou,E))
+    VL = copy.((v1,v2,v3))
+    UR = Fluxes1D.primitive_to_conservative(1.1,.2,2.1)
+    VR = Fluxes1D.v_ufun(UR...)
+    QL = Fluxes1D.conservative_to_primitive_beta(UL...)
+    QR = Fluxes1D.conservative_to_primitive_beta(UR...)
+    Fx = Fluxes1D.euler_fluxes_1D(QL...,QR...)
+    Fx2 = Fluxes1D.euler_fluxes_1D(QR...,QL...)
+    @test all(Fx .≈ Fx2)
+
+    # test consistency
+    p = Fluxes1D.pfun(rho,rhou,E)
+    exact_flux_x = (rho*u, rho*u^2 + p, u*(E+p))
+    FxL = Fluxes1D.euler_fluxes_1D(QL...,QL...)
+    @test all(FxL .≈ exact_flux_x)
+
+    # test entropy conservation property
+    # entropy potentials
+    ψx(U) = (γ-1)*U[2]*entropy_scale
+    vTFx = sum(((x,y,z)->((x-y)*z)).(VL,VR,Fx))
+    @test vTFx ≈ ψx(UL)-ψx(UR)
+end
+
 @testset "2D entropy variable tests" begin
     using EntropyStableEuler.Fluxes2D
-    import EntropyStableEuler.Fluxes2D: γ
+    import EntropyStableEuler: γ,entropy_scale
 
     rho,u,v,p = 1,.1,.2,2
-    rho,rhou,rhov,E = primitive_to_conservative(rho,u,v,p)
-    v1,v2,v3,v4 = v_ufun(rho,rhou,rhov,E)
+    rho,rhou,rhov,E = Fluxes2D.primitive_to_conservative(rho,u,v,p)
+    v1,v2,v3,v4 = Fluxes2D.v_ufun(rho,rhou,rhov,E)
 
     h = 1e-7
     central_diff(f,x) = (f(x+h) - f(x-h))/(2*h)
 
-    @test abs(v1 - central_diff(rho->Sfun(rho,rhou,rhov,E),rho)) < h
-    @test abs(v2 - central_diff(rhou->Sfun(rho,rhou,rhov,E),rhou)) < h
-    @test abs(v3 - central_diff(rhov->Sfun(rho,rhou,rhov,E),rhov)) < h
-    @test abs(v4 - central_diff(E->Sfun(rho,rhou,rhov,E),E)) < h
+    @test abs(v1 - central_diff(rho->Fluxes2D.Sfun(rho,rhou,rhov,E),rho)) < h
+    @test abs(v2 - central_diff(rhou->Fluxes2D.Sfun(rho,rhou,rhov,E),rhou)) < h
+    @test abs(v3 - central_diff(rhov->Fluxes2D.Sfun(rho,rhou,rhov,E),rhov)) < h
+    @test abs(v4 - central_diff(E->Fluxes2D.Sfun(rho,rhou,rhov,E),E)) < h
 
-    u1,u2,u3,u4 = u_vfun(v1,v2,v3,v4)
+    u1,u2,u3,u4 = Fluxes2D.u_vfun(v1,v2,v3,v4)
     @test u1 ≈ rho
     @test u2 ≈ rhou
     @test u3 ≈ rhov
@@ -33,12 +76,12 @@ end
     # test symmetry
     UL = copy.((rho,rhou,rhov,E))
     VL = copy.((v1,v2,v3,v4))
-    UR = primitive_to_conservative(1.1,.2,.3,2.1)
-    VR = v_ufun(UR...)
-    QL = conservative_to_primitive_beta(UL...)
-    QR = conservative_to_primitive_beta(UR...)
-    Fx,Fy = euler_fluxes_2D(QL...,QR...)
-    Fx2,Fy2 = euler_fluxes_2D(QR...,QL...)
+    UR = Fluxes2D.primitive_to_conservative(1.1,.2,.3,2.1)
+    VR = Fluxes2D.v_ufun(UR...)
+    QL = Fluxes2D.conservative_to_primitive_beta(UL...)
+    QR = Fluxes2D.conservative_to_primitive_beta(UR...)
+    Fx,Fy = Fluxes2D.euler_fluxes_2D(QL...,QR...)
+    Fx2,Fy2 = Fluxes2D.euler_fluxes_2D(QR...,QL...)
     @test all(Fx .≈ Fx2)
     @test all(Fy .≈ Fy2)
 
@@ -46,18 +89,78 @@ end
     p = Fluxes2D.pfun(rho,rhou,rhov,E)
     exact_flux_x = (rho*u, rho*u^2 + p, rhou*v, u*(E+p))
     exact_flux_y = (rho*v, rhou*v, rho*v^2 + p, v*(E+p))
-    FxL,FyL = euler_fluxes_2D(QL...,QL...)
+    FxL,FyL = Fluxes2D.euler_fluxes_2D(QL...,QL...)
     @test all(FxL .≈ exact_flux_x)
     @test all(FyL .≈ exact_flux_y)
 
     # test entropy conservation property
     # entropy potentials
-    ψx(U) = (γ-1)*U[2]*Fluxes2D.entropy_scale
-    ψy(U) = (γ-1)*U[3]*Fluxes2D.entropy_scale
+    ψx(U) = (γ-1)*U[2]*entropy_scale
+    ψy(U) = (γ-1)*U[3]*entropy_scale
     vTFx = sum(((x,y,z)->((x-y)*z)).(VL,VR,Fx))
     vTFy = sum(((x,y,z)->((x-y)*z)).(VL,VR,Fy))
     @test vTFx ≈ ψx(UL)-ψx(UR)
     @test vTFy ≈ ψy(UL)-ψy(UR)
+end
+
+
+@testset "3D entropy variable tests" begin
+    using EntropyStableEuler.Fluxes3D
+    import EntropyStableEuler: γ,entropy_scale
+
+    rho,u,v,w,p = 1,.1,.2,.3,2
+    rho,rhou,rhov,rhow,E = Fluxes3D.primitive_to_conservative(rho,u,v,w,p)
+    v1,v2,v3,v4,v5 = Fluxes3D.v_ufun(rho,rhou,rhov,rhow,E)
+
+    h = 1e-7
+    central_diff(f,x) = (f(x+h) - f(x-h))/(2*h)
+    @test abs(v1 - central_diff(rho->Fluxes3D.Sfun(rho,rhou,rhov,rhow,E),rho)) < h
+    @test abs(v2 - central_diff(rhou->Fluxes3D.Sfun(rho,rhou,rhov,rhow,E),rhou)) < h
+    @test abs(v3 - central_diff(rhov->Fluxes3D.Sfun(rho,rhou,rhov,rhow,E),rhov)) < h
+    @test abs(v4 - central_diff(rhow->Fluxes3D.Sfun(rho,rhou,rhov,rhow,E),rhow)) < h
+    @test abs(v5 - central_diff(E->Fluxes3D.Sfun(rho,rhou,rhov,rhow,E),E)) < h
+
+    u1,u2,u3,u4,u5 = Fluxes3D.u_vfun(v1,v2,v3,v4,v5)
+    @test u1 ≈ rho
+    @test u2 ≈ rhou
+    @test u3 ≈ rhov
+    @test u4 ≈ rhow
+    @test u5 ≈ E
+
+    # test symmetry
+    UL = copy.((rho,rhou,rhov,rhow,E))
+    VL = copy.((v1,v2,v3,v4,v5))
+    UR = Fluxes3D.primitive_to_conservative(1.1,.2,.3,.4,2.1)
+    VR = Fluxes3D.v_ufun(UR...)
+    QL = Fluxes3D.conservative_to_primitive_beta(UL...)
+    QR = Fluxes3D.conservative_to_primitive_beta(UR...)
+    Fx,Fy,Fz = Fluxes3D.euler_fluxes_3D(QL...,QR...)
+    Fx2,Fy2,Fz2 = Fluxes3D.euler_fluxes_3D(QR...,QL...)
+    @test all(Fx .≈ Fx2)
+    @test all(Fy .≈ Fy2)
+    @test all(Fz .≈ Fz2)
+
+    # test consistency
+    p = Fluxes3D.pfun(rho,rhou,rhov,rhow,E)
+    exact_flux_x = (rho*u, rho*u^2 + p, rhou*v,      rhou*w,      u*(E+p))
+    exact_flux_y = (rho*v, rhov*u,      rho*v^2 + p, rhov*w,      v*(E+p))
+    exact_flux_z = (rho*w, rhow*u,      rhow*v,      rho*w^2 + p, w*(E+p))
+    FxL,FyL,FzL = Fluxes3D.euler_fluxes_3D(QL...,QL...)
+    @test all(FxL .≈ exact_flux_x)
+    @test all(FyL .≈ exact_flux_y)
+    @test all(FzL .≈ exact_flux_z)
+
+    # test entropy conservation property
+    # entropy potentials
+    ψx(U) = (γ-1)*U[2]*entropy_scale
+    ψy(U) = (γ-1)*U[3]*entropy_scale
+    ψz(U) = (γ-1)*U[4]*entropy_scale
+    vTFx = sum(((x,y,z)->((x-y)*z)).(VL,VR,Fx))
+    vTFy = sum(((x,y,z)->((x-y)*z)).(VL,VR,Fy))
+    vTFz = sum(((x,y,z)->((x-y)*z)).(VL,VR,Fz))
+    @test vTFx ≈ ψx(UL)-ψx(UR)
+    @test vTFy ≈ ψy(UL)-ψy(UR)
+    @test vTFz ≈ ψz(UL)-ψz(UR)
 end
 
 # module bar
