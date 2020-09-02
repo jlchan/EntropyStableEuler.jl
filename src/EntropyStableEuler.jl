eb6cc640e53f7f8003e1957469163c0858c273b2
Jesse Chan <jesse.chan@rice.edu>
2020-09-02 17:00:53 -0500

    change default entropy scaling

--- src/EntropyStableEuler.jl
+++ src/EntropyStableEuler.jl
@@ -20,7 +20,7 @@ export wavespeed_1D
 
 # export entropy_scaling,scale_entropy_input,scale_entropy_output
 # changes definition of entropy variables by a constant scaling
-const entropy_scaling = 1/(γ-1)
+const entropy_scaling = 1/γ # implies vE = -1 /(ι * γ) and γι = non-dim temperature if R* = cp
 scale_entropy_output(V...) = (x->@. x*entropy_scaling).(V)
 scale_entropy_input(V...) = (x->@. x/entropy_scaling).(V)
 
